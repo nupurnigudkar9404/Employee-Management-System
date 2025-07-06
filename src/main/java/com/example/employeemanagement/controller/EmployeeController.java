4e7b9b397cfd36731ef94ea733b01e371b798a30
nupurnigudkar9404 <nupur.nigudkar9404@gmail.com>
2025-07-06 15:09:26 +0530

    save new employee

--- src/main/java/com/example/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/example/employeemanagement/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EmployeeController {
@@ -19,12 +21,17 @@ public class EmployeeController {
         return "index";
     }
 
-    @GetMapping("/")
-    public String showNewEmployee(Model model){
+    @GetMapping("/showNewEmployeeForm")
+    public String showNewEmployeeForm(Model model){
         Employee employee = new Employee();
-        model.addAttribute("employeee",employee);
+        model.addAttribute("employee",employee);
         return "new_employees";
     }
 
 
+    @PostMapping("/saveEmployees")
+    public String saveEmployees(@ModelAttribute("employee")Employee employee){
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
 }
