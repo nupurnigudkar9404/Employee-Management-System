fddab8c2d99e4b5738bbf6854235d62f41adfad7
nupurnigudkar9404 <nupur.nigudkar9404@gmail.com>
2025-07-06 21:01:55 +0530

    update delete options

--- src/main/java/com/example/employeemanagement/controller/EmployeeController.java
+++ src/main/java/com/example/employeemanagement/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -28,10 +29,25 @@ public class EmployeeController {
         return "new_employees";
     }
 
-
     @PostMapping("/saveEmployees")
     public String saveEmployees(@ModelAttribute("employee")Employee employee){
         employeeService.saveEmployee(employee);
         return "redirect:/";
     }
+
+    @GetMapping("/EmployeeUpdate/{id}")
+    public String showUpdate(@PathVariable (value = "id") long id, Model model){
+        Employee employee =employeeService.getEmployeeById(id);
+        model.addAttribute("employee",employee);
+        return "update_employee";
+    }
+
+    @GetMapping("/EmployeeDelete/{id}")
+    public String deleteEmployee(@PathVariable (value = "id") long id){
+        this.employeeService.deleteEmployeeById(id);
+        return "redirect:/";
+    }
 }
+
+
+
--- src/main/java/com/example/employeemanagement/service/EmployeeServiceImplementation.java
+++ src/main/java/com/example/employeemanagement/service/EmployeeServiceImplementation.java
@@ -6,10 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImplementation implements EmployeeService{
-
 
     @Autowired
     private EmployeeRepository employeeRepository;
@@ -22,6 +22,23 @@ public class EmployeeServiceImplementation implements EmployeeService{
     @Override
     public void saveEmployee(Employee employee) {
         this.employeeRepository.save(employee);
+    }
 
+    @Override
+    public Employee getEmployeeById(long id) {
+        Optional<Employee> optional =employeeRepository.findById(id);
+        Employee employee= null;
+        if(optional.isPresent()){
+            employee = optional.get();
+        }
+        else {
+            throw new RuntimeException("Employee not found by id : "+ id);
+        }
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployeeById(long id) {
+        this.employeeRepository.deleteById(id);
     }
 }
